7e65577c1d8e93871ae9abf81012eeae3165c9a4
njmercado <njmercado@uninorte.edu.co>
2021-01-02 19:05:41 -0500

    Added endpoint to suggest passwords

--- routers/signup_router.py
+++ routers/signup_router.py
@@ -1,15 +1,38 @@
 from fastapi import APIRouter, HTTPException
 from schemas.signup_schema import SignupSchema
 from utils.queries import user_consumer
+from utils.password import generatePassword
 
 router = APIRouter()
 
 @router.post("/")
 def signup(signup_data: SignupSchema):
 
-  response = user_consumer.create_user(signup_data)
+  try:
+    
+    response = user_consumer.create_user(signup_data)
 
-  return {
-    "message": response,
-    "error": None
-  }
+    return {
+      "message": response["message"],
+      "error": response["error"]
+    }
+  except Exception as error:
+    return {
+      "message": f"Some error has happend: {error}",
+      "error": True
+    }
+
+@router.get("/")
+def suggestPassword():
+  try:
+    password = generatePassword(10)
+
+    return {
+      "message": password,
+      "error": False
+    }
+  except Exception as error:
+    return {
+      "message": f"Some error has happend: {error}",
+      "error": True
+    }
