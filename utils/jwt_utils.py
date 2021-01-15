3df720c4659bd8a626d29e92717d7a6a5b2bde4c
njmercado <njmercado@uninorte.edu.co>
2021-01-15 17:51:49 -0500

    Agregada función para verificar el token

--- utils/jwt_utils.py
+++ utils/jwt_utils.py
@@ -1,12 +1,15 @@
 from jose import JWTError, jwt
 from environs import Env
 from datetime import datetime, timedelta
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from utils.queries.user_consumer import verifyUser
 
 env = Env()
 env.read_env()
 
 SECRET_KEY = env("SECRET_KEY")
-HASH_ALGORITHM="HS256"
+HASH_ALGORITHM = "HS256"
 
 def generateJWT(jwt_data, alive_days=8):
   day_of_expiration = datetime.utcnow() + timedelta(days=alive_days)
@@ -17,3 +20,13 @@ def generateJWT(jwt_data, alive_days=8):
     "message": encoded_jwt,
     "error": False
   }
+
+def verifyJWT(token: str):
+  try:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[HASH_ALGORITHM])
+    if verifyUser(payload):
+      return { "message": "Success", "error": False }
+    else:
+      return {"message": "Some error has happend, are you trying to fuck me?", "error": True}
+  except Exception as error:
+    return {"message": f"Some error has happend: {error}", "error": True}
