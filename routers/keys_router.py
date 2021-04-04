664513953a0756923fd807f1b27e68d824dfba70
njmercado <njmercado@uninorte.edu.co>
2021-04-04 01:24:34 -0500

    Minor changes

--- routers/keys_router.py
+++ routers/keys_router.py
@@ -41,19 +41,34 @@ def getAllKeys(
     user_email = jwt_response["data"]["email"]
     enterprise_id = uuid.UUID(enterprise_id)
     project_id = uuid.UUID(project_id)
-    envs = getKeys(enterprise_id=enterprise_id, project_id=project_id, env_id=env_id, user_email = user_email)
+    envs = getKeys(
+      enterprise_id=enterprise_id, 
+      project_id=project_id,
+      env_id=env_id,
+      user_email = user_email
+    )
     return envs 
   except Exception as error:
     return { "message": f"Some error has happend in getAllKeys at keys_router: {error}", "error": True}
 
 @router.get("/only")
-def getOneKey(enterprise_id: str, project_id: str, env_id: str, jwt_response: dict = Depends(jwtAccess)):
+def getOneKey(
+  enterprise_id: str,
+  project_id: str,
+  env_id: str,
+  jwt_response: dict = Depends(jwtAccess)
+):
   try:
     user_email = jwt_response["data"]["email"]
     enterprise_id = uuid.UUID(enterprise_id)
     project_id = uuid.UUID(project_id)
     env_id = uuid.UUID(env_id)
-    project = getKey(enterprise_id=enterprise_id, project_id=project_id, env_id=env_id, user_email=user_email)
+    project = getKey(
+      enterprise_id=enterprise_id,
+      project_id=project_id,
+      env_id=env_id,
+      user_email=user_email
+    )
     return project
   except Exception as error:
     return {"message": f"Some error has happend: {error}", "error": True}
